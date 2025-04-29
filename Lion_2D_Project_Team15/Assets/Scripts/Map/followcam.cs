301c8d80b722846ff7b0fab7badfc48df553df18
정영호 <gggrhrh98@naver.com>
2025-04-29 18:19:39 +0900

    cinemachine수정

--- Lion_2D_Project_Team15/Assets/Scripts/Map/followcam.cs
+++ Lion_2D_Project_Team15/Assets/Scripts/Map/followcam.cs
@@ -7,6 +7,11 @@ public class followcam : MonoBehaviour
 
     public void transCam(int index)
     {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+
+        //카메라 타겟 설정
+        cinemachineCamera[index].Follow = player.transform;
+
         cinemachineCamera[index].gameObject.SetActive(true);
 
         for (int i = 0; i < cinemachineCamera.Length; i++)
