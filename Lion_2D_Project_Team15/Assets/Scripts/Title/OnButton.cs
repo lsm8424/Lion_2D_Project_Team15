e6bb50490091c752a390ccf791a7fe630342710a
정영호 <gggrhrh98@naver.com>
2025-05-12 01:23:43 +0900

    타이틀 버튼

--- Lion_2D_Project_Team15/Assets/Scripts/Title/OnButton.cs
+++ Lion_2D_Project_Team15/Assets/Scripts/Title/OnButton.cs
@@ -9,33 +9,35 @@ public class OnButton : MonoBehaviour
     public void OnClick()
     {
         //텍스트 색깔 변경
-        ButtonImage.color = new Color(0.7f, 0.7f, 0.7f, 1); // 투명도 변경
+        ButtonImage.color = new Color(0.7f,0.7f,0, 1); // 투명도 변경
     }
 
     public void OutClick()
     {
         //텍스트 색깔 변경
-        ButtonImage.color = new Color(1, 1, 1, 1); // 투명도 변경
+        ButtonImage.color = new Color(1, 1, 0, 1); // 투명도 변경
     }
 
     public void StartButtonClick()
     {
         Debug.Log("버튼 클릭됨(씬이동)");
 
-        //캔버스 비활성화
-        Canvas canvas = GetComponentInParent<Canvas>();
-        canvas.sortingOrder = -2;
-
+        Invoke("OutClick", 0.2f); // 클릭 후 0.1초 뒤에 OutClick() 호출
+        
         SceneController.Instance.LoadSceneWithFadeInOut("Prologue2", 3f);
     }
 
     public void SettingButtonClick()
     {
+        Invoke("OutClick", 0.2f); // 클릭 후 0.1초 뒤에 OutClick() 호출
+
         UIManager.Instance.OpenPanel(UIManager.Instance.VolumePanel);
     }
 
     public void ExitButtonClick()
     {
+        Invoke("OutClick", 0.2f); // 클릭 후 0.1초 뒤에 OutClick() 호출
+
         Debug.Log("게임 종료");
         //Application.Quit();
     }
