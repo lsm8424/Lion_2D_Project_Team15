26f3dd97cce3b7f009f1d508bdb4166f2596b5c8
정영호 <gggrhrh98@naver.com>
2025-05-26 14:24:52 +0900

    체력에 따른 빨간색

--- Lion_2D_Project_Team15/Assets/Scripts/UI/Player/Player_UI.cs
+++ Lion_2D_Project_Team15/Assets/Scripts/UI/Player/Player_UI.cs
@@ -1,9 +1,14 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class Player_UI : MonoBehaviour
 {
     [SerializeField] Slider _healthBar;
+    [SerializeField] Volume volume;
+    private Vignette vignette;
 
     Player _player;
 
@@ -27,6 +32,12 @@ public class Player_UI : MonoBehaviour
         {
             _healthBar = GetComponentInChildren<Slider>();
         }
+
+        if (volume.profile.TryGet(out Vignette v))
+        {
+            vignette = v;
+            vignette.color.value = Color.red; // 빨간색 설정
+        }
     }
 
     void Start()
@@ -36,8 +47,12 @@ public class Player_UI : MonoBehaviour
         if (_player != null)
         {
             _player.OnDamaged += UpdateHealthBar;
+            _player.OnDamaged += UpdateVolume;
             UpdateHealthBar();
+            UpdateVolume();
         }
+
+       
     }
 
     void UpdateHealthBar()
@@ -46,5 +61,20 @@ public class Player_UI : MonoBehaviour
         {
             _healthBar.value = _player.HP;
         }
+    }
+
+    void UpdateVolume()
+    {
+        if (vignette == null)
+            return;
+
+        float hpPercent = _player.HP / _player.maxHP;
+
+        if(hpPercent < 0.3f)
+            vignette.intensity.value = 0.5f; // 체력이 20% 이하일 때 강한 효과
+        else if(hpPercent < 0.5f)
+            vignette.intensity.value = 0.3f; // 체력이 40% 이하일 때 중간 효과
+        else if(hpPercent < 0.7f)
+            vignette.intensity.value = 0.1f; // 그 외에는 약한 효과
     }
 }
